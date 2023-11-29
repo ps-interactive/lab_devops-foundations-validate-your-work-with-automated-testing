ceaf16787311c955af25398a2436d2749dd6dc14
Stefan Nychka <stefan-nychka@pluralsight.com>
2023-11-29 16:10:55 -0700

    add in Chris' starter code

---
+++ EmployeeManager/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeManager
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ EmployeeManager/EmployeeManager.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeManager
+{
+    public static class EmployeeManager
+    {
+        public static string GetEmployeeFriendlyName(Employee employee)
+        {
+            return employee.FirstName + employee.LastName;
+        }
+    }
+}
